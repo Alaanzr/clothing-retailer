f283249c3aa0210f149b358bc232fac92ca91dd6
Alaanzr <Alaanzr@gmail.com>
2016-02-03 16:04:20 +0000

    Refactored code and added functionality to determine the categories existant in the user's cart

--- app/js/itemController.js
+++ app/js/itemController.js
@@ -2,20 +2,16 @@ retailerApp.controller('ItemCtrl', ['$scope', 'Item', function($scope, Item) {
 
   $scope.items = Item.query();
   $scope.total = 0;
-
+  $scope.categoriesInCart = [];
+  $scope.discountActive = false;
   $scope.cart = [];
 
   $scope.addItem = function(item) {
-    if(item.quantityInStock > 0) {
-      $scope.total += item.price;
-      item.quantityInStock -=1;
+      $scope.modifyItemProps(item);
       $scope.modifyCartAdd(item);
-    };
   };
 
   $scope.removeItem = function(item) {
-    var index = $scope.cart.indexOf(item);
-    $scope.cart.splice(index, 1);
     $scope.modifyCartRm(item);
   };
 
@@ -25,6 +21,24 @@ retailerApp.controller('ItemCtrl', ['$scope', 'Item', function($scope, Item) {
 
   // PRIVATE
 
+  $scope.modifyItemProps = function(item) {
+    item.quantityInStock -=1;
+    $scope.isInCart(item) ? item.quantity += 1 : item.quantity = 1;
+  };
+
+  $scope.modifyCartAdd = function(item) {
+      $scope.total += item.price;
+      $scope.categoriesInCart.push(item.category);
+      $scope.cart.push(item);
+  };
+
+  $scope.modifyCartRm = function(item) {
+    $scope.cart.splice($scope.cart.indexOf(item), 1);
+    item.quantityInStock += item.quantity;
+    $scope.total -= item.price * item.quantity;
+    item.quantity = 0;
+  };
+
   $scope.isInCart = function(item) {
     for (var i = 0; i < $scope.cart.length; i++) {
       if ($scope.cart[i].productName === item.productName) {
@@ -33,18 +47,10 @@ retailerApp.controller('ItemCtrl', ['$scope', 'Item', function($scope, Item) {
     };
   };
 
-  $scope.modifyCartAdd = function(item) {
-    if ($scope.isInCart(item)) {
-      item.quantity += 1;
-    } else {
-      item.quantity = 1;
-      $scope.cart.push(item);
-    }
-  };
-
-  $scope.modifyCartRm = function(item) {
-    item.quantityInStock += item.quantity;
-    $scope.total -= item.price * item.quantity;
-    item.quantity = 0;
+  $scope.reviewCartCategories = function() {
+    var args = Array.prototype.slice.call(arguments, 0);
+    return $scope.categoriesInCart.some(function(elm) {
+      return args.indexOf(elm) !== -1;
+    });
   };
 }]);
