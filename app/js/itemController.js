62eb23eb9af92efa88c4daa4fcc3e3a7e0b05ae2
Alaanzr <Alaanzr@gmail.com>
2016-02-03 22:24:38 +0000

    Users can now redeem vouchers

--- app/js/itemController.js
+++ app/js/itemController.js
@@ -2,9 +2,9 @@ retailerApp.controller('ItemCtrl', ['$scope', 'Item', function($scope, Item) {
 
   $scope.items = Item.query();
   $scope.total = 0;
+  $scope.discount = 0;
   $scope.cart = [];
   $scope.categoriesInCart = [];
-  $scope.discountActive = false;
 
   $scope.addItem = function(item) {
     $scope.modifyItemPropsAdd(item);
@@ -18,7 +18,24 @@ retailerApp.controller('ItemCtrl', ['$scope', 'Item', function($scope, Item) {
 
   $scope.isInStock = function(item) {
     return item.quantityInStock > 0 ? true : false;
-  }
+  };
+
+  $scope.applyDiscount = function(code) {
+    var discountConditions = {'FIVE': true, 'TEN': $scope.total > 50, 'FIFTEEN': $scope.total > 75 && $scope.reviewCartCategories('Men\'s Footwear', 'Women\'s Footwear')};
+    if (discountConditions.hasOwnProperty(code)) {
+      for (var key in discountConditions) {
+        if (key === code) {
+          if (discountConditions[code] === true && $scope.discount === 0) {
+            if ($scope.total > 5 * (Object.keys(discountConditions).indexOf(code) + 1)) {
+              $scope.discount += 5 * (Object.keys(discountConditions).indexOf(code) + 1);
+              $scope.total -= 5 * (Object.keys(discountConditions).indexOf(code) + 1);
+              $scope.discountCode = '';
+            };
+          };
+        }
+      };
+    };
+  };
 
   // PRIVATE
 
@@ -35,7 +52,8 @@ retailerApp.controller('ItemCtrl', ['$scope', 'Item', function($scope, Item) {
 
   $scope.modifyCartRm = function(item) {
     $scope.cart.splice($scope.cart.indexOf(item), 1);
-    $scope.total -= item.price * item.quantityInCart;
+    $scope.total -= (item.price * item.quantityInCart) - $scope.discount;
+    $scope.discount = 0;
   };
 
   $scope.modifyItemPropsRm = function(item) {
--- test/unit/itemController.spec.js
+++ test/unit/itemController.spec.js
@@ -1,7 +1,13 @@
 describe('ItemCtrl', function() {
 
   var scope, ctrl, $httpBackend;
-  var item1 = {productName: 'Suede Shoes, Blue', category: 'Women\'s Footwear', price: 42.00, quantityInStock: 4}
+  var item1 = {
+    productName: 'Suede Shoes, Blue',
+    category: 'Women\'s Footwear',
+    price: 42.00,
+    quantityInStock: 4
+  };
+  var item2;
 
   beforeEach(function() {
     jasmine.addMatchers({
@@ -23,7 +29,9 @@ describe('ItemCtrl', function() {
     $httpBackend = _$httpBackend_;
     $httpBackend.expectGET('items/items.json').respond([item1]);
     scope = $rootScope.$new();
-    ctrl = $controller('ItemCtrl', {$scope:scope});
+    ctrl = $controller('ItemCtrl', {
+      $scope: scope
+    });
   }));
 
   it('should not contain item data prior to calling the Item Service', function() {
@@ -47,47 +55,106 @@ describe('ItemCtrl', function() {
       expect(scope.total).toBe(0);
     });
 
-      describe('Adding Items', function() {
+    describe('Adding Items', function() {
+
+      beforeEach(function() {
+        item1 = {
+          productName: 'Suede Shoes, Blue',
+          category: 'Women\'s Footwear',
+          price: 42.00,
+          quantityInStock: 4
+        }
+        scope.addItem(item1);
+      });
+
+      it('should show items in the cart after adding said item', function() {
+        expect(scope.cart).toEqualData([item1]);
+      });
+
+      it('should add to the customer\'s total', function() {
+        expect(scope.total).toBe(42);
+      });
+
+      it('should reduce the quantityInStock for the associated item', function() {
+        expect(item1.quantityInStock).toBe(3);
+      });
+
+      describe('Removing Items', function() {
 
         beforeEach(function() {
-          item1 = {productName: 'Suede Shoes, Blue', category: 'Women\'s Footwear', price: 42.00, quantityInStock: 4}
-          scope.addItem(item1);
+          scope.removeItem(item1);
         });
 
-        it('should show items in the cart after adding said item', function() {
-          expect(scope.cart).toEqualData([item1]);
+        it('should remove the relevant item from the cart', function() {
+          expect(scope.cart.length).toBe(0);
         });
 
-        it('should add to the customer\'s total', function() {
-          expect(scope.total).toBe(42);
+        it('should reduce the order total', function() {
+          expect(scope.total).toBe(0);
         });
 
-        it('should reduce the quantityInStock for the associated item', function() {
-          expect(item1.quantityInStock).toBe(3);
+        it('should revert the item\'s quantityInStock back to its original value', function() {
+          expect(item1.quantityInStock).toBe(4);
         });
 
-        describe('Removing Items', function() {
-
-          beforeEach(function() {
-            scope.removeItem(item1);
-          });
-
-          it('should remove the relevant item from the cart', function() {
-            expect(scope.cart.length).toBe(0);
-          });
-
-          it('should reduce the order total', function() {
-            expect(scope.total).toBe(0);
-          });
-
-          it('should revert the item\'s quantityInStock back to its original value', function() {
-            expect(item1.quantityInStock).toBe(4);
-          });
-
-          it('should revert the item quantity back to 0', function() {
-            expect(item1.quantityInCart).toBe(0);
-          });
+        it('should revert the item quantity back to 0', function() {
+          expect(item1.quantityInCart).toBe(0);
         });
       });
     });
+
+    describe('Applying Discounts', function() {
+
+      beforeEach(function() {
+        item2 = {
+          productName: 'Gold Button Cardigan, Black',
+          category: 'Women\'s Casualwear',
+          price: 167.00,
+          quantityInStock: 6
+        }
+      });
+
+      it('should not allow the user to apply a discount when their order total is £0', function() {
+        scope.applyDiscount('FIVE');
+        expect(scope.discount).toBe(0);
+      });
+
+      it('should apply a £5 discount when the correct code is used', function() {
+        scope.addItem(item2);
+        scope.applyDiscount('FIVE');
+        expect(scope.discount).toBe(5);
+        expect(scope.total).toBe(162);
+      });
+
+      it('should not apply a £10 discount when the order total is below £50', function() {
+        scope.addItem(item1);
+        scope.applyDiscount('TEN');
+        expect(scope.discount).toBe(0);
+        expect(scope.total).toBe(42);
+      });
+
+      it('should not apply a £15 discount when the order total is below £75 and footwear is not in the user\'s cart', function() {
+        scope.addItem(item2);
+        scope.applyDiscount('FIFTEEN');
+        expect(scope.discount).toBe(0);
+        expect(scope.total).toBe(167);
+      });
+
+      it('should apply a £15 discount when the order total is above £75 and footwear is included in the user\'s cart', function() {
+        scope.addItem(item1);
+        scope.addItem(item2);
+        scope.applyDiscount('FIFTEEN');
+        expect(scope.discount).toBe(15);
+        expect(scope.total).toBe(194);
+      });
+
+      it('should not allow the discount to persist when a user removes an item from their cart', function() {
+        scope.addItem(item1);
+        scope.applyDiscount('FIVE');
+        scope.removeItem(item1);
+        expect(scope.discount).toBe(0);
+        expect(scope.total).toBe(0);
+      });
+    });
+  });
 });
